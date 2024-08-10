39d5e265b471d72d51f7e7cb7f68c8bdd3ff0c3a
Kalyan Kanchumarthi <109733865+SrinivasaKalyan@users.noreply.github.com>
2024-08-10 16:22:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,21 +11,7 @@ lottie_animation = load_lottie_file("ani.json")
 lottie_animation1 = load_lottie_file("dolphin.json")
 
 
-def create_copy_button(text_to_copy):
-    button_id = "copyButton" + text_to_copy
-    
-    button_html = f"""<button id="{button_id}">Copy</button>
-    <script>
-    document.getElementById("{button_id}").onclick = function() {{
-        navigator.clipboard.writeText("{text_to_copy}").then(function() {{
-            console.log('Async: Copying to clipboard was successful!');
-        }}, function(err) {{
-            console.error('Async: Could not copy text: ', err);
-        }});
-    }}
-    </script>"""
-    
-    st.markdown(button_html, unsafe_allow_html=True)
+
 
 
 def main():
